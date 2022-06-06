3df1d7636e81646fbce5b24abc2678382576a8dd
Yongmalyang <37902200+Yongmalyang@users.noreply.github.com>
2022-06-06 23:09:24 +0900

    선호도 체크 화면

--- lib/Preferences/Preferences.dart
+++ lib/Preferences/Preferences.dart
@@ -1,15 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'Preferences_good.dart';
+import 'Preferences_notbad.dart';
+import 'Preferences_never.dart';
 
 class Preferences extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Preferences"),
-      ),
-      body: Center(
-        child: Text('Preferences'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Preferences"),
+        ),
+        body: ListView(
+            padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 30),
+            scrollDirection:
+                Axis.vertical, //vertical : 수직으로 나열 / horizontal : 수평으로 나열
+
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(bottom: 30),
+                child: ButtonTheme(
+                  height: 120,
+                  child: RaisedButton(
+                    child: Text(
+                      'GOOD',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                      ),
+                    ),
+                    color: Color(0xffdce5ee),
+                    onPressed: () {
+                      print('Good');
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Preferences_good(
+                                  title: '',
+                                )),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 30),
+                child: ButtonTheme(
+                  height: 120,
+                  child: RaisedButton(
+                    child: Text(
+                      'NOT BAD',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                      ),
+                    ),
+                    color: Color(0xfffdfce0),
+                    onPressed: () {
+                      print('Not Bad');
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Preferences_notbad(
+                                  title: '',
+                                )),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Container(
+                  margin: const EdgeInsets.only(bottom: 30),
+                  child: ButtonTheme(
+                    height: 120,
+                    child: RaisedButton(
+                      child: Text(
+                        'NEVER',
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.black,
+                        ),
+                      ),
+                      color: Color(0xffe8dad9),
+                      onPressed: () {
+                        print('Never');
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Preferences_never(
+                                    title: '',
+                                  )),
+                        );
+                      },
+                    ),
+                  ))
+            ]));
   }
 }
+
+/*
+child: RaisedButton(
+                child: Text('Good'),
+                textColor: Colors.white,
+                color: Colors.black,
+                onPressed: () {
+                  print('Good');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Preferences_good(
+                              title: '',
+                            )),
+                  );
+                },
+              ),*/
---
+++ lib/Preferences/Preferences_never.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class Preferences_never extends StatefulWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Preferences_never"),
+      ),
+      body: Center(
+        child: Text('Preferences_never'),
+      ),
+    );
+  }
+
+  Preferences_never({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _CheckBoxInListViewState createState() => _CheckBoxInListViewState();
+}
+
+class _CheckBoxInListViewState extends State<Preferences_never> {
+  //bool _isChecked = false;
+
+  List<String> _texts = [
+    "설거지하기",
+    "세탁기 돌리기",
+    "빨래 널기",
+    "빨래 개기",
+    "청소기 돌리기",
+    "물걸레질 하기",
+    "식사 준비(수저 놓기, 반찬 꺼내기 등)",
+    "음식 만들기(요리, 조리)",
+    "분리수거하기",
+    "음식물쓰레기 버리기"
+  ];
+
+  late List<bool> _isChecked;
+
+  @override
+  void initState() {
+    super.initState();
+    _isChecked = List<bool>.filled(_texts.length, false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Preferences_never'),
+        ),
+        body: Column(children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _texts.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: CheckboxListTile(
+                    title: Text(_texts[index]),
+                    value: _isChecked[index],
+                    onChanged: (val) {
+                      setState(
+                        () {
+                          _isChecked[index] = val!;
+                          print(index);
+                        },
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ]));
+  }
+}
